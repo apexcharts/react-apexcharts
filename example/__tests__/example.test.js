324df6041f9a573fe4f706a2931b5e4fdf79cd32
Juned Chhipa <juned.chhipa@gmail.com>
2018-08-26 17:16:33 +0530

    updated to apexcharts@1.3.12 - to allow jest tests to run

--- example/__tests__/example.test.js
+++ example/__tests__/example.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Area from '../src/chart-types/Area';
 import renderer from 'react-test-renderer';
 
-test('Link changes the class when hovered', () => {
+// This test was solely created to confirm the svgjs undefined issue was fixed.
+// The core apexcharts library test suite contains the major test cases
+test('Example test to fix svgjs issues', () => {
   const component = renderer.create(
     <Area></Area>,
   );
