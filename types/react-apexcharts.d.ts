a692e0c2900837b477a4a263b4b2010719d6b28d
Juned Chhipa <juned.chhipa@gmail.com>
2019-03-25 01:00:05 +0530

    types file - removed comments

--- types/react-apexcharts.d.ts
+++ types/react-apexcharts.d.ts
@@ -7,11 +7,9 @@ declare module "react-apexcharts" {
     interface Props {
         type?: "line" | "area" | "bar" | "histogram" | "pie" | "donut" |
             "radialBar" | "scatter" | "bubble" | "heatmap" | "candlestick" | "radar",
-        // TODO Use more specific series types as defined here https://apexcharts.com/docs/series/
         series?: Array<any>,
         width?: string | number,
         height?: string | number,
-        // TODO Use more specific options types for each chart type
         options?: object
     }
 
