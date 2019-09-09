dfbe603fb20ac8e9d0178a6d1110d09c418b352d
Bikram <6bikramsutar@gmail.com>
2019-09-09 22:36:31 +0530

    updated gulp to version 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,42 @@
-var gulp = require('gulp');
-var react = require('gulp-react');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var babel = require('gulp-babel');
+const { src, dest, series, parallel } = require('gulp');
+const react = require('gulp-react');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
 
-gulp.task('dev', function () {
-  return gulp.src(['./src/react-apexcharts.jsx'])
-    .pipe(react({
-      es6module: true
-    }))
-    .pipe(concat('react-apexcharts.js'))
-    .pipe(gulp.dest('./dist/'));
-});
+function dev() {
+  return (
+    src(['./src/react-apexcharts.jsx'])
+      .pipe(react({
+        es6module: true
+      }))
+      .pipe(concat('react-apexcharts.js'))
+      .pipe(dest('./dist/'))
+  );
+}
 
-gulp.task('prod', function () {
-  return gulp.src(['./src/react-apexcharts.jsx'])
-    .pipe(react({
-      es6module: true
-    }))
-    .pipe(babel({
-      presets: ['es2015', 'react'],
-      plugins: ['transform-object-rest-spread']
-    }))
-    .pipe(concat('react-apexcharts.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/'));
-});
+function prod() {
+  return (
+    src(['./src/react-apexcharts.jsx'])
+      .pipe(react({
+        es6module: true
+      }))
+      .pipe(babel({
+        presets: ['es2015', 'react'],
+        plugins: ['transform-object-rest-spread']
+      }))
+      .pipe(concat('react-apexcharts.min.js'))
+      .pipe(uglify())
+      .pipe(dest('./dist/'))
+  );
+}
 
-gulp.task('types', function () {
-  return gulp.src(['./types/react-apexcharts.d.ts'])
-    .pipe(gulp.dest('./dist/'))
-})
+function types() {
+  return (
+    src(['./types/react-apexcharts.d.ts'])
+      .pipe(dest('./dist/'))
+  );
+}
 
-gulp.task('build', ['dev', 'prod', 'types']);
-gulp.task('default', ['dev', 'prod', 'types']);
+exports.build = parallel(dev, prod, types);
+exports.default = series(dev, prod, types);
