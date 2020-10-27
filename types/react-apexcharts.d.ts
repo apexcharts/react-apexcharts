6dc632efa96c3e90fbe46173de026c834fd0e7a5
junedchhipa <juned.chhipa@gmail.com>
2020-10-28 00:27:44 +0530

    add type treemap; fixes #232

--- types/react-apexcharts.d.ts
+++ types/react-apexcharts.d.ts
@@ -24,6 +24,7 @@ declare module "react-apexcharts" {
         | "scatter"
         | "bubble"
         | "heatmap"
+        | "treemap"
         | "candlestick"
         | "radar"
         | "polarArea"
