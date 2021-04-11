98191b53ce7cd22ff633337387fe4bcf32febbf4
junedchhipa <juned.chhipa@gmail.com>
2021-04-11 23:14:00 +0530

    add missing type

--- types/react-apexcharts.d.ts
+++ types/react-apexcharts.d.ts
@@ -25,6 +25,7 @@ declare module "react-apexcharts" {
         | "bubble"
         | "heatmap"
         | "treemap"
+        | "boxPlot"
         | "candlestick"
         | "radar"
         | "polarArea"
