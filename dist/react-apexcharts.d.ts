0bc33735ce449203fe2683f26e7640dc02a0e9d8
junedchhipa <juned.chhipa@gmail.com>
2021-04-12 17:38:29 +0530

    build - 1.3.9

--- dist/react-apexcharts.d.ts
+++ dist/react-apexcharts.d.ts
@@ -4,31 +4,28 @@ import { ApexOptions } from 'apexcharts'
  * Basic type definitions from https://apexcharts.com/docs/react-charts/#props
  */
 declare module "react-apexcharts" {
-    interface Props {
-        type?: ChartType,
-        series?: Array<any>,
-        width?: string | number,
-        height?: string | number,
-        options?: ApexOptions,
-        [key: string]: any,
-    }
-      
-      export type ChartType =
-        | "line"
-        | "area"
-        | "bar"
-        | "histogram"
-        | "pie"
-        | "donut"
-        | "radialBar"
-        | "scatter"
-        | "bubble"
-        | "heatmap"
-        | "treemap"
-        | "boxPlot"
-        | "candlestick"
-        | "radar"
-        | "polarArea"
-        | "rangeBar";
-
+  interface Props {
+    type?: "line"
+    | "area"
+    | "bar"
+    | "histogram"
+    | "pie"
+    | "donut"
+    | "radialBar"
+    | "scatter"
+    | "bubble"
+    | "heatmap"
+    | "treemap"
+    | "boxPlot"
+    | "candlestick"
+    | "radar"
+    | "polarArea"
+    | "rangeBar",
+    series?: Array<any>,
+    width?: string | number,
+    height?: string | number,
+    options?: ApexOptions,
+    [key: string]: any,
+  }
+  export default class ReactApexChart extends React.Component<Props> {}    
 }
