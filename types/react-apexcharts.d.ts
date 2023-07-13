e4643d2e2cc1e24dee88d3ecdd21279d94f80846
junedchhipa <juned.chhipa@gmail.com>
2023-07-13 21:58:24 +0530

    update types to include rangeArea

--- types/react-apexcharts.d.ts
+++ types/react-apexcharts.d.ts
@@ -1,37 +1,33 @@
 /// <reference types="react"/>
-import { ApexOptions } from 'apexcharts';
-import React from 'react';
+import { ApexOptions } from "apexcharts";
+import React from "react";
 /**
  * Basic type definitions from https://apexcharts.com/docs/react-charts/#props
  */
 declare module "react-apexcharts" {
   export interface Props {
-    type: "line"
-    | "area"
-    | "bar"
-    | "histogram"
-    | "pie"
-    | "donut"
-    | "radialBar"
-    | "scatter"
-    | "bubble"
-    | "heatmap"
-    | "treemap"
-    | "boxPlot"
-    | "candlestick"
-    | "radar"
-    | "polarArea"
-    | "rangeBar",
-    series: ApexOptions['series'],
-    width?: string | number,
-    height?: string | number,
-    options: ApexOptions,
-    [key: string]: any,
+    type?:
+      | "line"
+      | "area"
+      | "bar"
+      | "pie"
+      | "donut"
+      | "radialBar"
+      | "scatter"
+      | "bubble"
+      | "heatmap"
+      | "candlestick"
+      | "boxPlot"
+      | "radar"
+      | "polarArea"
+      | "rangeBar"
+      | "rangeArea"
+      | "treemap";
+    series?: ApexOptions["series"];
+    width?: string | number;
+    height?: string | number;
+    options?: ApexOptions;
+    [key: string]: any;
   }
-  export default class ReactApexChart extends React.Component<Props> {
-    render(): React.DetailedReactHTMLElement<{
-      children?: React.ReactNode;
-      ref: React.RefObject<any> | ((el: any) => any) | undefined;
-    }, any>
-  }
+  export default class ReactApexChart extends React.Component<Props> {}
 }
